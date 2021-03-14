206ff43a23c2544fa2b0788ac266f6e687b582ef
ItzStingray <70822304+ItzStingray@users.noreply.github.com>
2021-03-14 13:31:12 +1100

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float moveSpeed = 100f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0, 0, moveSpeed);
+
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(40, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-40, 0, 0);
+        }
+    }
+}
